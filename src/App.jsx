e83bee8235612205773c4da286ec0e5c938766fc
贾丽鹏 <1094425279@qq.com>
2026-01-12 14:34:40 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useImmer } from "use-immer";
+import "./App.css";
+
+import ModalTest from "./components/custom-modal-popup/modal-test";
+import TabTest from "./components/cuttom-tabs/tab-test";
+
+function App() {
+  return (
+    <div>
+      <h1>欢迎来到我的应用</h1>
+      {/* <ModalTest />*/}
+      <TabTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cuttom-tabs/tab-test.jsx
@@ -0,0 +1,29 @@
+import Tabs from "./Tab";
+import "./tabs.css";
+
+function RandomComponent() {
+  return <h3>Some random content</h3>;
+}
+
+export default function TabTest() {
+  const tabs = [
+    {
+      label: "Tab 1",
+      content: <div>This is content for Tab 1</div>,
+    },
+    {
+      label: "Tab 2",
+      content: <div>This is content for Tab 2</div>,
+    },
+    {
+      label: "Tab 3",
+      content: <RandomComponent />,
+    },
+  ];
+
+  function handleChange(currentTabIndex) {
+    console.log(currentTabIndex);
+  }
+
+  return <Tabs tabsContent={tabs} onChange={handleChange} />;
+}
