4dae08070024c01c67f2fd73cff56c1c33548de7
贾丽鹏 <1094425279@qq.com>
2026-01-13 20:00:56 +0800

    Code is temporarily saved.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import "./App.css";
 
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/cuttom-tabs/tab-test";
+import Accordian from "./components/accordian/Accordian";
+import GithubProfileFinder from "./components/github-profile-finder/GithubProfileFinder";
 
 function App() {
   return (
     <div>
       <h1>欢迎来到我的应用</h1>
       {/* <ModalTest />*/}
-      <TabTest />
+      {/* <TabTest />*/}
+      {/* <Accordian />*/}
+      <GithubProfileFinder />
     </div>
   );
 }
